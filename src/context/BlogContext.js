c3bbe1f86f92dcf7956f52d2363ab99f41c2fc1b
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2021-07-05 21:37:38 -0400

    Passing down context, Using reducer in context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useState, useReducer} from 'react';
 
 const BlogContext = React.createContext();
 
+const ADD = 'add';
+const EDIT = 'edit';
+const DELETE = 'delete';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case ADD:
+            return [...state, action.payload];
+        case EDIT:
+            return state;
+        case DELETE:
+            return state;
+        default:
+            return state;
+    }
+};
+
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider>
+
+    const [blogPosts, dispatch] = useReducer(reducer, []);
+
+    const addBlogPost = () => {
+        dispatch({type: ADD, payload: {title: `Blog Post #${blogPosts.length + 1}`}})
+    };
+    
+
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
         {children}
     </BlogContext.Provider>
 };
+
+export default BlogContext;
